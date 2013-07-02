f3cc93f49c3d028bc68ae507ffb5396bf58bec86
jimmyli <jyl702@gmail.com>
2013-07-02 12:17:34 -0400

    flesh out level and state managers

--- level_manager.js
+++ level_manager.js
@@ -1,17 +1,20 @@
 ;(function(exports) {
-  var LevelManager = function(game, levelNum, levelFn) {
+  var LevelManager = function(game, canvas) {
     this.game = game;
-    this.game.levels = this.game.levels || {};
+    this.levels = {};
   };
 
+  LevelManager.prototype.currentLevelFn = function () {
+    return this.levels[this.currentLevelNum];
+  }
+
   LevelManager.prototype.addLevel = function(levelNum, levelFn) {
-    this.game.levels[levelNum] = levelFn;
+    this.levels[levelNum] = levelFn;
   }
 
   LevelManager.prototype.removeLevel = function(levelNum, stateName) {
-    delete this.game.levels[levelNum];
+    delete this.levels[levelNum];
   }
-
 
   exports.LevelManager = LevelManager;
 })(this);
--- state_manager.js
+++ state_manager.js
@@ -1,8 +1,12 @@
 ;(function(exports) {
-  var StateManager = function(game, canvas) {
+  var StateManager = function(game, canvas, initialState) {
     this.game = game;
     this.states = {};
   };
+
+  StateManager.prototype.currentStateFn = function () {
+    return this.states[this.currentStateFn];
+  }
 
   StateManager.prototype.addState = function(stateName, stateFn) {
     this.states[stateName] = stateFn;
